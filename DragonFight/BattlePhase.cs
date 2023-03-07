7d8b81d182590405bcc932b06a02c250a625db5d
nategoodman88 <nategoodman88@gmail.com>
2023-03-07 12:22:40 -0500

    finalized battle. program complete, but stats need balancing after more QA

--- DragonFight/BattlePhase.cs
+++ DragonFight/BattlePhase.cs
@@ -11,16 +11,13 @@ namespace DragonFight
     public class BattlePhase : EquipPhase
     {
         //declare the variables for the dragon attributes as well as variables needed to run through the battle including whose turn it is, amount of damage done, etc. 
-        static int dragonDefense = 15;
+        static int dragonDefense = 10;
         static int dragonHealth = 100;
-        static int dragonAttack = 5;
+        static int dragonAttack = 10;
 
         public static void doBattlePhase(CharacterCreate c, EquipPhase e)
         {
             int initHP = c.hp;
-            Console.WriteLine("Yeahhhh we batttlingggggg ah ah");
-            Console.WriteLine(c.hp); //debugging to make sure variable sticks
-            Console.WriteLine(e.primaryWep); //debugging to make sure variable sticks
             //Bool and while loop to go through the battle until there is a winner
             bool battling = true;
             string turn = "";
@@ -38,118 +35,19 @@ namespace DragonFight
                     if (turnDecider == 1)
                     {
                         turn = "Player";
-                    }
-                    else
-                    {
-                        turn = "Dragon";
-                    }
-                }
-                if (turn == "Player")
-                {
-                    Console.WriteLine(c.name + "'s turn is next");
-                    Console.WriteLine("What would you like to do? Type the number for your chosen action and hit enter.\n1.Attack\n2.Magic\n3.Fortify and heal.");
-                    bool choosingAction = true;
-                    while (choosingAction)
-                    {
-                        string move = Console.ReadLine();
-                        if (move == "1")
-                        {
-                            int damage = (c.hit * random.Next(0, 5)) - dragonDefense;
-                            Console.WriteLine(damage);
-                            dragonHealth -= damage;
-                            if (dragonHealth < 0)
-                            {
-                                dragonHealth = 0;
-                            }
-                            Console.WriteLine(c.name + " hits Edel for " + damage + " damage. Edel's HP is now " + dragonHealth);
-                            choosingAction = false;
-                            if (dragonHealth == 0)
-                            {
-                                Console.WriteLine(c.name + " has killed Edel! You win!");
-                                battling = false;
-                            }
-                        }
-                        if (move == "2")
-                        {
-                            int damage = (c.magic * random.Next(0, 5)) - dragonDefense;
-                            dragonHealth -= damage;
-                            if (dragonHealth < 0)
-                            {
-                                dragonHealth = 0;
-                            }
-                            Console.WriteLine(c.name + " hits Edel for " + damage + " damage. Edel's HP is now " + dragonHealth);
-                            choosingAction = false;
-                            if (dragonHealth == 0)
-                            {
-                                Console.WriteLine(c.name + " has killed Edel! You win!");
-                                battling = false;
-                            }
-                        }
-                        if (move == "3")
-                        {
-                            if (c.hp < initHP)
-                            {
-                                c.hp += random.Next(5, 10);
-                                if (c.hp > initHP)
-                                {
-                                    c.hp = initHP;
-                                }
-                                Console.WriteLine("You fortify and recover some health. Your HP is now: " + c.hp);
-                                choosingAction = false;
-                            }
-                            else
-                            {
-                                Console.WriteLine("You are already at full health!");
-                                choosingAction = true;
-                            }
-                            choosingAction = false;
-                        }
-                    }
-                    if (turn == "Dragon")
-                    {
-                        Console.WriteLine("Edel prepares to attack!");
-                        int damage = (dragonAttack * random.Next(0, 2)) - c.defense;
-                        c.hp -= damage;
-                        if (c.hp < 0)
-                        {
-                            c.hp = 0;
-                        }
-                        Console.WriteLine("Edel hits " + c.name + " for " + damage + " damage. " + c.name + "'s HP is now " + c.hp);
-                        if (c.hp == 0)
-                        {
-                            Console.WriteLine("Edel has won the battle. Game over");
-                            battling = false;
-                        }
-                    }
-                    if (turn == "Player")
-                    {
-                        turn = "Dragon";
-                        Console.WriteLine("Edel prepares to attack!");
-                        int damage = (dragonAttack * random.Next(0, 2)) - c.defense;
-                        c.hp -= damage;
-                        if (c.hp < 0)
-                        {
-                            c.hp = 0;
-                        }
-                        Console.WriteLine("Edel hits " + c.name + " for " + damage + " damage. " + c.name + "'s HP is now " + c.hp);
-                        if (c.hp == 0)
-                        {
-                            Console.WriteLine("Edel has won the battle. Game over");
-                            battling = false;
-                        }
-                    }
-                    if (turn == "Dragon")
-                    {
                         Console.WriteLine(c.name + "'s turn is next");
                         Console.WriteLine("What would you like to do? Type the number for your chosen action and hit enter.\n1.Attack\n2.Magic\n3.Fortify and heal.");
-                        choosingAction = true;
+                        bool choosingAction = true;
                         while (choosingAction)
                         {
                             string move = Console.ReadLine();
                             if (move == "1")
                             {
                                 int damage = (c.hit * random.Next(0, 5)) - dragonDefense;
-                                Console.WriteLine(damage);
+                                if (damage < 0)
+                                {
+                                    damage = 0;
+                                }
                                 dragonHealth -= damage;
                                 if (dragonHealth < 0)
                                 {
@@ -166,6 +64,10 @@ namespace DragonFight
                             if (move == "2")
                             {
                                 int damage = (c.magic * random.Next(0, 5)) - dragonDefense;
+                                if (damage < 0)
+                                {
+                                    damage = 0;
+                                }
                                 dragonHealth -= damage;
                                 if (dragonHealth < 0)
                                 {
@@ -199,14 +101,33 @@ namespace DragonFight
                                 choosingAction = false;
                             }
                         }
-                        //debugging
-                        Console.WriteLine(turn);
-                        Console.WriteLine("butts");
-                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        turn = "Dragon";
+                        Console.WriteLine("Edel prepares to attack!");
+                        int damage = (dragonAttack * random.Next(1, 4)) - c.defense;
+                        if (damage < 0)
+                        {
+                            damage = 0;
+                        }
+                        c.hp -= damage;
+                        if (c.hp < 0)
+                        {
+                            c.hp = 0;
+                        }
+                        Console.WriteLine("Edel hits " + c.name + " for " + damage + " damage. " + c.name + "'s HP is now " + c.hp);
+                        if (c.hp == 0)
+                        {
+                            Console.WriteLine("Edel has won the battle. Game over");
+                            battling = false;
+                        }
+                    }
                     }
                 }
             }
         }
     }
-}
+
+
 
--- DragonFight/Program.cs
+++ DragonFight/Program.cs
@@ -12,21 +12,35 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
+        bool programRunning = true;
         //print dragon art, banner, story, run rest of program methods, delay further methods until story is done
         DragonFight.Story.St();
         await Task.Delay(25000);
-        //empty to allow further access of variables
-        CharacterCreate x = new();
-        //lets user create character choosing name, class and race with unqiue attributes, also lets user start over if they are not satisfied 
-        DragonFight.CharacterCreate.Create(x);
-        //empty to allow further access of variables
-        EquipPhase y = new();
-        //chooses equipment for the upcoming battle
-        DragonFight.EquipPhase.Equip(x,y);
-        //initiate the battle with dragon
-        DragonFight.BattlePhase.doBattlePhase(x,y);
-        
-        Console.ReadLine(); //debugging
+        //while loop so user can play multiple times
+        while (programRunning) 
+        {
+            //empty to allow further access of variables
+            CharacterCreate x = new();
+            //lets user create character choosing name, class and race with unqiue attributes, also lets user start over if they are not satisfied 
+            DragonFight.CharacterCreate.Create(x);
+            //empty to allow further access of variables
+            EquipPhase y = new();
+            //chooses equipment for the upcoming battle
+            DragonFight.EquipPhase.Equip(x, y);
+            //initiate the battle with dragon
+            DragonFight.BattlePhase.doBattlePhase(x, y);
+            //ask if they want to play again
+            Console.WriteLine("Would you like to play again? Enter Y or N");
+            string answer = Console.ReadLine();
+            if (answer.ToUpper() == "Y")
+            {
+                programRunning = true;
+            }
+            else
+            {
+                programRunning = false;
+            }
+        }
     }
 
 }
